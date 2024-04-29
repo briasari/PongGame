59b46704333ac1864f5f335363b0a71c4249f784
briasari <briasari660@gmail.com>
2024-04-29 14:59:02 -0400

    finished

--- PongGame/Form1.cs
+++ PongGame/Form1.cs
@@ -13,21 +13,28 @@ namespace PongGame
     public partial class Form1 : Form
     {
 
-        Rectangle player1 = new Rectangle(10, 170, 10, 60);
-        Rectangle player2 = new Rectangle(580, 170, 10, 60);
+        Rectangle player1 = new Rectangle(10, 140, 10, 60);
+        Rectangle player2 = new Rectangle(10, 210, 10, 60);
+        Rectangle border = new Rectangle(8, 208, 14, 64);
         Rectangle ball = new Rectangle(295, 195, 10, 10);
 
         int player1Score = 0;
         int player2Score = 0;
 
         int playerSpeed = 4;
-        int ballXSpeed = -6;
+        int ballXSpeed = 6;
         int ballYSpeed = -6;
 
+        int round = 1;
+
         bool wPressed = false;
+        bool aPressed = false;
         bool sPressed = false;
+        bool dPressed = false;
         bool upPressed = false;
         bool downPressed = false;
+        bool leftPressed = false;
+        bool rightPressed = false;
 
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
@@ -46,14 +53,26 @@ namespace PongGame
                 case Keys.W:
                     wPressed = true;
                     break;
+                case Keys.A:
+                    aPressed = true;
+                    break;
                 case Keys.S:
                     sPressed = true;
+                    break;
+                case Keys.D:
+                    dPressed = true;
                     break;
                 case Keys.Up:
                     upPressed = true;
                     break;
                 case Keys.Down:
                     downPressed = true;
+                    break;
+                case Keys.Left:
+                    leftPressed = true;
+                    break;
+                case Keys.Right:
+                    rightPressed = true;
                     break;
             }
         }
@@ -65,14 +84,26 @@ namespace PongGame
                 case Keys.W:
                     wPressed = false;
                     break;
+                case Keys.A:
+                    aPressed = false;
+                    break;
                 case Keys.S:
                     sPressed = false;
+                    break;
+                case Keys.D:
+                    dPressed = false;
                     break;
                 case Keys.Up:
                     upPressed = false;
                     break;
                 case Keys.Down:
                     downPressed = false;
+                    break;
+                case Keys.Left:
+                    leftPressed = false;
+                    break;
+                case Keys.Right:
+                    rightPressed = false;
                     break;
             }
         }
@@ -100,6 +131,14 @@ namespace PongGame
             {
                 player1.Y = player1.Y + playerSpeed;
             }
+            if (dPressed == true && player1.X < this.Width - player1.Width)
+            {
+                player1.X = player1.X + playerSpeed;
+            }
+            if (aPressed == true && player1.X > 0)
+            {
+                player1.X = player1.X - playerSpeed;
+            }
 
             //move player2
             if (upPressed == true && player2.Y > 0)
@@ -110,21 +149,60 @@ namespace PongGame
             {
                 player2.Y = player2.Y + playerSpeed;
             }
-
-            //check if the ball hit 1player
-            if (player1.IntersectsWith(ball))
+            if (rightPressed == true && player1.X < this.Width - player2.Width)
             {
-                ballXSpeed = -ballXSpeed;
-                ball.X = player1.X + player1.Width;
+                player2.X = player2.X + playerSpeed;
             }
+            if (leftPressed == true && player2.X > 0)
+            {
+                player2.X = player2.X - playerSpeed;
+            }
+
+            //check for intersection with border depending on round
+            if (round % 2 == 0)
+            {
+                border.X = player2.X - 2;
+                border.Y = player2.Y - 2;
+
+                if (border.IntersectsWith(ball))
+                {
+                    ballXSpeed = -ballXSpeed;
+                    ball.X = player2.X + player2.Width;
+                    round++;
+                }
+            }
+            else
+            {
+                border.X = player1.X -2;
+                border.Y = player1.Y -2;
+
+                if (border.IntersectsWith(ball))
+                {
+                    ballXSpeed = -ballXSpeed;
+                    ball.X = player1.X + player1.Width;
+                    round++;
+                }
+            }
+
+
 
             //check if ball goes off left side of screen
             if (ball.X <= 0)
             {
-                player2Score++;
-                p2ScoreLabel.Text = $"{player2Score}";
-                ball.X = 295;
-                ball.Y = 195;
+                if (border.X == player2.X - 2)
+                {
+                    player1Score++;
+                    p1ScoreLabel.Text = $"{player1Score}";
+                    ball.X = 295;
+                    ball.Y = 195;
+                }
+                else
+                {
+                    player2Score++;
+                    p2ScoreLabel.Text = $"{player2Score}";
+                    ball.X = 295;
+                    ball.Y = 195;
+                }
 
                 int randValue = randGen.Next(1, 3);
                 if (randValue == 1)
@@ -138,35 +216,12 @@ namespace PongGame
             }
 
             //check if ball goes off right side of screen
-            if (ball.X >= this.Width - ball.Width)
-            {
-                player1Score++;
-                p1ScoreLabel.Text = $"{player1Score}";
-                ball.X = 295;
-                ball.Y = 195;
-
-                int randValue = randGen.Next(1, 3);
-                if (randValue == 1)
-                {
-                    ballYSpeed = -6;
-                }
-                else
-                {
-                    ballYSpeed = 6;
-                }
-            }
-
-            if (player2.IntersectsWith(ball))
+            if (ball.X == this.Width)
             {
                 ballXSpeed = -ballXSpeed;
-                ball.X = player2.X - player2.Width;
             }
 
-            if (player1.IntersectsWith(ball))
-            {
-                ballXSpeed = -ballXSpeed;
-                ball.X = player1.X + player1.Width;
-            }
+
 
             //check for winner
             if (player1Score == 3)
@@ -186,6 +241,7 @@ namespace PongGame
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            e.Graphics.FillRectangle(whiteBrush, border);
             e.Graphics.FillRectangle(blueBrush, player1);
             e.Graphics.FillRectangle(blueBrush, player2);
             e.Graphics.FillRectangle(whiteBrush, ball);
