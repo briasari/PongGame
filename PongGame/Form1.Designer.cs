75fe574f63c1924afa022a105de3742f542ff21b
briasari660 <briasari660@SDS-L-F8QLG63>
2025-02-03 14:27:48 -0500

    one sided pong altered

--- PongGame/Form1.Designer.cs
+++ PongGame/Form1.Designer.cs
@@ -46,9 +46,10 @@
             this.p1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
             this.p1ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.p1ScoreLabel.ForeColor = System.Drawing.Color.White;
-            this.p1ScoreLabel.Location = new System.Drawing.Point(133, 9);
+            this.p1ScoreLabel.Location = new System.Drawing.Point(177, 11);
+            this.p1ScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.p1ScoreLabel.Name = "p1ScoreLabel";
-            this.p1ScoreLabel.Size = new System.Drawing.Size(100, 23);
+            this.p1ScoreLabel.Size = new System.Drawing.Size(133, 28);
             this.p1ScoreLabel.TabIndex = 0;
             this.p1ScoreLabel.Text = "0";
             this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -58,9 +59,10 @@
             this.p2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
             this.p2ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.p2ScoreLabel.ForeColor = System.Drawing.Color.White;
-            this.p2ScoreLabel.Location = new System.Drawing.Point(360, 9);
+            this.p2ScoreLabel.Location = new System.Drawing.Point(480, 11);
+            this.p2ScoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.p2ScoreLabel.Name = "p2ScoreLabel";
-            this.p2ScoreLabel.Size = new System.Drawing.Size(100, 23);
+            this.p2ScoreLabel.Size = new System.Drawing.Size(133, 28);
             this.p2ScoreLabel.TabIndex = 1;
             this.p2ScoreLabel.Text = "0";
             this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -70,24 +72,26 @@
             this.winLabel.BackColor = System.Drawing.Color.Transparent;
             this.winLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.winLabel.ForeColor = System.Drawing.Color.White;
-            this.winLabel.Location = new System.Drawing.Point(137, 133);
+            this.winLabel.Location = new System.Drawing.Point(183, 164);
+            this.winLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.winLabel.Name = "winLabel";
-            this.winLabel.Size = new System.Drawing.Size(331, 23);
+            this.winLabel.Size = new System.Drawing.Size(441, 28);
             this.winLabel.TabIndex = 2;
-            this.winLabel.Text = "----";
+            this.winLabel.Text = "Press [Space] To Start";
             this.winLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.ClientSize = new System.Drawing.Size(800, 492);
             this.Controls.Add(this.winLabel);
             this.Controls.Add(this.p2ScoreLabel);
             this.Controls.Add(this.p1ScoreLabel);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
--- PongGame/Form1.cs
+++ PongGame/Form1.cs
@@ -35,6 +35,9 @@ namespace PongGame
         bool downPressed = false;
         bool leftPressed = false;
         bool rightPressed = false;
+        bool spacePressed = false;
+
+        bool gameInSession = false;
 
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
@@ -74,6 +77,9 @@ namespace PongGame
                 case Keys.Right:
                     rightPressed = true;
                     break;
+                case Keys.Space:
+                    spacePressed = true;
+                    break;
             }
         }
 
@@ -105,138 +111,151 @@ namespace PongGame
                 case Keys.Right:
                     rightPressed = false;
                     break;
+                case Keys.Space:
+                    spacePressed = false;
+                    break;
             }
         }
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-            //move ball
-            ball.X = ball.X + ballXSpeed;
-            ball.Y = ball.Y + ballYSpeed;
-
-            if (ball.Y <= 0 || ball.Y >= this.Height - ball.Height)
+            if (spacePressed == true)
             {
-                ballYSpeed = -ballYSpeed;
-            }
-            if (ball.X <= 0 || ball.X >= this.Width - ball.Width)
-            {
-                ballXSpeed = -ballXSpeed;
+                gameInSession = true;
+                winLabel.Text = "----";
             }
 
-            //move player1
-            if (wPressed == true && player1.Y > 0)
+            if (gameInSession == true)
             {
-                player1.Y = player1.Y - playerSpeed;
-            }
-            if (sPressed == true && player1.Y < this.Height - player1.Height)
-            {
-                player1.Y = player1.Y + playerSpeed;
-            }
-            if (dPressed == true && player1.X < this.Width - player1.Width)
-            {
-                player1.X = player1.X + playerSpeed;
-            }
-            if (aPressed == true && player1.X > 0)
-            {
-                player1.X = player1.X - playerSpeed;
-            }
 
-            //move player2
-            if (upPressed == true && player2.Y > 0)
-            {
-                player2.Y = player2.Y - playerSpeed;
-            }
-            if (downPressed == true && player2.Y < this.Height - player2.Height)
-            {
-                player2.Y = player2.Y + playerSpeed;
-            }
-            if (rightPressed == true && player1.X < this.Width - player2.Width)
-            {
-                player2.X = player2.X + playerSpeed;
-            }
-            if (leftPressed == true && player2.X > 0)
-            {
-                player2.X = player2.X - playerSpeed;
-            }
+                //move ball
+                ball.X = ball.X + ballXSpeed;
+                ball.Y = ball.Y + ballYSpeed;
 
-            //check for intersection with border depending on round
-            if (round % 2 == 0)
-            {
-                border.X = player2.X - 2;
-                border.Y = player2.Y - 2;
-
-                if (border.IntersectsWith(ball))
+                if (ball.Y <= 0 || ball.Y >= this.Height - ball.Height)
+                {
+                    ballYSpeed = -ballYSpeed;
+                }
+                if (ball.X <= 0 || ball.X >= this.Width - ball.Width)
                 {
                     ballXSpeed = -ballXSpeed;
-                    ball.X = player2.X + player2.Width;
-                    round++;
                 }
-            }
-            else
-            {
-                border.X = player1.X -2;
-                border.Y = player1.Y -2;
 
-                if (border.IntersectsWith(ball))
+                //move player1
+                if (wPressed == true && player1.Y > 0)
                 {
-                    ballXSpeed = -ballXSpeed;
-                    ball.X = player1.X + player1.Width;
-                    round++;
+                    player1.Y = player1.Y - playerSpeed;
                 }
-            }
-
-
-
-            //check if ball goes off left side of screen
-            if (ball.X <= 0)
-            {
-                if (border.X == player2.X - 2)
+                if (sPressed == true && player1.Y < this.Height - player1.Height)
                 {
-                    player1Score++;
-                    p1ScoreLabel.Text = $"{player1Score}";
-                    ball.X = 295;
-                    ball.Y = 195;
+                    player1.Y = player1.Y + playerSpeed;
+                }
+                if (dPressed == true && player1.X < this.Width - player1.Width)
+                {
+                    player1.X = player1.X + playerSpeed;
+                }
+                if (aPressed == true && player1.X > 0)
+                {
+                    player1.X = player1.X - playerSpeed;
+                }
+
+                //move player2
+                if (upPressed == true && player2.Y > 0)
+                {
+                    player2.Y = player2.Y - playerSpeed;
+                }
+                if (downPressed == true && player2.Y < this.Height - player2.Height)
+                {
+                    player2.Y = player2.Y + playerSpeed;
+                }
+                if (rightPressed == true && player1.X < this.Width - player2.Width)
+                {
+                    player2.X = player2.X + playerSpeed;
+                }
+                if (leftPressed == true && player2.X > 0)
+                {
+                    player2.X = player2.X - playerSpeed;
+                }
+
+                //check for intersection with border depending on round
+                if (round % 2 == 0)
+                {
+                    border.X = player2.X - 2;
+                    border.Y = player2.Y - 2;
+
+                    if (border.IntersectsWith(ball))
+                    {
+                        ballXSpeed = -ballXSpeed;
+                        ball.X = player2.X + player2.Width;
+                        round++;
+                    }
                 }
                 else
                 {
-                    player2Score++;
-                    p2ScoreLabel.Text = $"{player2Score}";
-                    ball.X = 295;
-                    ball.Y = 195;
+                    border.X = player1.X - 2;
+                    border.Y = player1.Y - 2;
+
+                    if (border.IntersectsWith(ball))
+                    {
+                        ballXSpeed = -ballXSpeed;
+                        ball.X = player1.X + player1.Width;
+                        round++;
+                    }
                 }
 
-                int randValue = randGen.Next(1, 3);
-                if (randValue == 1)
+
+
+                //check if ball goes off left side of screen
+                if (ball.X <= 0)
                 {
-                    ballYSpeed = -6;
+                    if (border.X == player2.X - 2)
+                    {
+                        player1Score++;
+                        p1ScoreLabel.Text = $"{player1Score}";
+                        ball.X = 295;
+                        ball.Y = 195;
+                    }
+                    else
+                    {
+                        player2Score++;
+                        p2ScoreLabel.Text = $"{player2Score}";
+                        ball.X = 295;
+                        ball.Y = 195;
+                    }
+
+                    int randValue = randGen.Next(1, 3);
+                    if (randValue == 1)
+                    {
+                        ballYSpeed = -6;
+                    }
+                    else
+                    {
+                        ballYSpeed = 6;
+                    }
                 }
-                else
+
+                //check if ball goes off right side of screen
+                if (ball.X == this.Width)
                 {
-                    ballYSpeed = 6;
+                    ballXSpeed = -ballXSpeed;
                 }
+
+
+
+                //check for winner
+                if (player1Score == 3)
+                {
+                    winLabel.Text = "Player 1 Wins!";
+                    gameTimer.Stop();
+                }
+
+                if (player2Score == 3)
+                {
+                    winLabel.Text = "Player 2 Wins!";
+                    gameTimer.Stop();
+                }
+
+                Refresh();
             }
-
-            //check if ball goes off right side of screen
-            if (ball.X == this.Width)
-            {
-                ballXSpeed = -ballXSpeed;
-            }
-
-
-
-            //check for winner
-            if (player1Score == 3)
-            {
-                winLabel.Text = "Player 1 Wins!";
-                gameTimer.Stop();
-            }
-
-            if (player2Score == 3)
-            {
-                winLabel.Text = "Player 2 Wins!";
-                gameTimer.Stop();
-            }
-
-            Refresh();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
